4577f2970d734a6713ec323155831bf5c643905c
cuong31012002 <95065030+cuong31012002@users.noreply.github.com>
2021-11-26 11:58:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Tâm à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,12 +7,12 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Em rất thích anh <33',
+    btnNo: 'Không, Em không thích anh :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh ❤',
     btnReply: 'Gửi cho anh ❤',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu anh nhiều lắm',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun ❤',
+    messDesc: 'Nhanh sang bên này với anh.',
+    btnAccept: 'Vâng ạ ❤',
     messLink: 'http://fb.com' //cuongg20cm.}
